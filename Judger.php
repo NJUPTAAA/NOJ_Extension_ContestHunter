5aa158ad7b974390a5b15439bf8c465ad02dbbde
David Diao <daviddiaoyf@gmail.com>
2019-07-16 05:19:36 +0800

    updated judger

--- Judger.php
+++ Judger.php
@@ -12,70 +12,55 @@ class Judger extends Curl
 {
 
     public $verdict=[
-        'Accepted'=>"Accepted",
-        "Presentation Error"=>"Presentation Error",
-        'Time Limit Exceeded'=>"Time Limit Exceed",
-        "Memory Limit Exceeded"=>"Memory Limit Exceed",
-        'Wrong Answer'=>"Wrong Answer",
-        'Runtime Error'=>"Runtime Error",
-        'Output Limit Exceeded'=>"Output Limit Exceeded",
-        'Compile Error'=>"Compile Error",
+        '正确'=>"Accepted",
+        '答案错误'=>"Wrong Answer",
+        '超出时间限制'=>"Time Limit Exceed",
+        '运行时错误'=>"Runtime Error",
+        "超出内存限制"=>"Memory Limit Exceed",
+        '比较器错误'=>'Submission Error',
+        '超出输出限制'=>"Output Limit Exceeded",
+        '编译错误'=>"Compile Error",
     ];
-    private $model=[];
-    private $template=[];
-
-
-    public function __construct()
-    {
-        $this->model["submissionModel"]=new SubmissionModel();
-        $this->model["judgerModel"]=new JudgerModel();
-    }
 
     public function judge($row)
     {
-        $sub=[];
-
-        if (!isset($this->poj[$row['remote_id']])) {
-            $judgerDetail=$this->model["judgerModel"]->detail($row['jid']);
-            $this->appendPOJStatus($judgerDetail['handle'], $row['remote_id']);
-            if (!isset($this->poj[$row['remote_id']])) {
+        try {
+            $sub=[];
+            $res=Requests::get('http://contest-hunter.org:83/record/'.$row['remote_id']);
+            preg_match('/<dt>状态<\/dt>[\s\S]*?<dd class=".*?">(.*?)<\/dd>/m', $res->body, $match);
+            $status=$match[1];
+            if (!array_key_exists($status, $contesthunter_v)) {
                 return;
             }
-        }
-
-        $status=$this->poj[$row['remote_id']];
-        $sub['verdict']=$this->verdict[$status['verdict']];
-
-        if ($sub['verdict']=='Compile Error') {
-            try {
-                $res=Requests::get('http://poj.org/showcompileinfo?solution_id='.$row['remote_id']);
-                preg_match('/<pre>([\s\S]*)<\/pre>/', $res->body, $match);
-                $sub['compile_info']=html_entity_decode($match[1], ENT_QUOTES);
-            } catch (Exception $e) {
+            $sub['verdict']=$contesthunter_v[$status];
+            $sub["score"]=$sub['verdict']=="Accepted" ? 1 : 0;
+            $sub['remote_id']=$row['remote_id'];
+            if ($sub['verdict']!="Submission Error" && $sub['verdict']!="Compile Error") {
+                preg_match('/占用内存[\s\S]*?(\d+).*?KiB/m', $res->body, $match);
+                $sub['memory']=$match[1];
+                $maxtime=0;
+                preg_match_all('/<span class="pull-right muted">(\d+) ms \/ \d+ KiB<\/span>/', $res->body, $matches);
+                foreach ($matches[1] as $time) {
+                    if ($time<$maxtime) {
+                        $maxtime=$time;
+                    }
+                }
+                $sub['time']=$maxtime;
+            } else {
+                $sub['memory']=0;
+                $sub['time']=0;
+                if ($sub['verdict']=='Compile Error') {
+                    preg_match('/<h2>结果 <small>各个测试点的详细结果<\/small><\/h2>\s*<pre>([\s\S]*?)<\/pre>/', $res->body, $match);
+                    $sub['compile_info']=html_entity_decode($match[1], ENT_QUOTES);
+                }
             }
-        }
 
-        $sub["score"]=$sub['verdict']=="Accepted" ? 1 : 0;
-        $sub['time']=$status['time'];
-        $sub['memory']=$status['memory'];
-        $sub['remote_id']=$row['remote_id'];
-
-        $this->model["submissionModel"]->updateSubmission($row['sid'], $sub);
-    }
-
-    private function appendPOJStatus($judger, $first=null)
-    {
-        if ($first!==null) {
-            $first++;
-        }
-        $res=Requests::get("http://poj.org/status?user_id={$judger}&top={$first}");
-        $rows=preg_match_all('/<tr align=center><td>(\d+)<\/td><td>.*?<\/td><td>.*?<\/td><td>.*?<font color=.*?>(.*?)<\/font>.*?<\/td><td>(\d*)K?<\/td><td>(\d*)(?:MS)?<\/td>/', $res->body, $matches);
-        for ($i=0; $i<$rows; $i++) {
-            $this->poj[$matches[1][$i]]=[
-                'verdict'=>$matches[2][$i],
-                'memory'=>$matches[3][$i] ? $matches[3][$i] : 0,
-                'time'=>$matches[4][$i] ? $matches[4][$i] : 0,
-            ];
+            // $ret[$row['sid']]=[
+            //     "verdict"=>$sub['verdict']
+            // ];
+            $submissionModel = new SubmissionModel();
+            $submissionModel->updateSubmission($row['sid'], $sub);
+        } catch (Exception $e) {
         }
     }
 }
