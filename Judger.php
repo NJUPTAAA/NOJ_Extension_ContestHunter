0124e21606eeb534814082f2f30e768817683121
David Diao <daviddiaoyf@gmail.com>
2019-07-21 04:48:09 +0800

    update web interface

--- Judger.php
+++ Judger.php
@@ -11,53 +11,53 @@ use Log;
 class Judger extends Curl
 {
 
-    public $verdict=[
-        '正确'=>"Accepted",
-        '答案错误'=>"Wrong Answer",
-        '超出时间限制'=>"Time Limit Exceed",
-        '运行时错误'=>"Runtime Error",
-        "超出内存限制"=>"Memory Limit Exceed",
-        '比较器错误'=>'Submission Error',
-        '超出输出限制'=>"Output Limit Exceeded",
-        '编译错误'=>"Compile Error",
+    public $verdict = [
+        '正确' => "Accepted",
+        '答案错误' => "Wrong Answer",
+        '超出时间限制' => "Time Limit Exceed",
+        '运行时错误' => "Runtime Error",
+        "超出内存限制" => "Memory Limit Exceed",
+        '比较器错误' => 'Submission Error',
+        '超出输出限制' => "Output Limit Exceeded",
+        '编译错误' => "Compile Error",
     ];
 
 
     public function __construct()
     {
-        $this->submissionModel=new SubmissionModel();
+        $this->submissionModel = new SubmissionModel();
     }
 
     public function judge($row)
     {
         try {
-            $sub=[];
-            $res=Requests::get('http://contest-hunter.org:83/record/'.$row['remote_id']);
+            $sub = [];
+            $res = Requests::get('http://contest-hunter.org:83/record/' . $row['remote_id']);
             preg_match('/<dt>状态<\/dt>[\s\S]*?<dd class=".*?">(.*?)<\/dd>/m', $res->body, $match);
-            $status=$match[1];
+            $status = $match[1];
             if (!array_key_exists($status, $verdict)) {
                 return;
             }
-            $sub['verdict']=$verdict[$status];
-            $sub["score"]=$sub['verdict']=="Accepted" ? 1 : 0;
-            $sub['remote_id']=$row['remote_id'];
-            if ($sub['verdict']!="Submission Error" && $sub['verdict']!="Compile Error") {
+            $sub['verdict'] = $verdict[$status];
+            $sub["score"] = $sub['verdict'] == "Accepted" ? 1 : 0;
+            $sub['remote_id'] = $row['remote_id'];
+            if ($sub['verdict'] != "Submission Error" && $sub['verdict'] != "Compile Error") {
                 preg_match('/占用内存[\s\S]*?(\d+).*?KiB/m', $res->body, $match);
-                $sub['memory']=$match[1];
-                $maxtime=0;
+                $sub['memory'] = $match[1];
+                $maxtime = 0;
                 preg_match_all('/<span class="pull-right muted">(\d+) ms \/ \d+ KiB<\/span>/', $res->body, $matches);
                 foreach ($matches[1] as $time) {
-                    if ($time<$maxtime) {
-                        $maxtime=$time;
+                    if ($time < $maxtime) {
+                        $maxtime = $time;
                     }
                 }
-                $sub['time']=$maxtime;
+                $sub['time'] = $maxtime;
             } else {
-                $sub['memory']=0;
-                $sub['time']=0;
-                if ($sub['verdict']=='Compile Error') {
+                $sub['memory'] = 0;
+                $sub['time'] = 0;
+                if ($sub['verdict'] == 'Compile Error') {
                     preg_match('/<h2>结果 <small>各个测试点的详细结果<\/small><\/h2>\s*<pre>([\s\S]*?)<\/pre>/', $res->body, $match);
-                    $sub['compile_info']=html_entity_decode($match[1], ENT_QUOTES);
+                    $sub['compile_info'] = html_entity_decode($match[1], ENT_QUOTES);
                 }
             }
 
--- Submitter.php
+++ Submitter.php
@@ -4,69 +4,90 @@ namespace App\Babel\Extension\contesthunter;
 use App\Babel\Submit\Curl;
 use App\Models\CompilerModel;
 use App\Models\JudgerModel;
+use App\Models\OJModel;
 use Illuminate\Support\Facades\Validator;
 use Requests;
 
 class Submitter extends Curl
 {
     protected $sub;
-    public $post_data=[];
+    public $post_data = [];
     protected $oid;
     protected $selectedJudger;
 
-    public function __construct(& $sub, $all_data)
+    public function __construct(&$sub, $all_data)
     {
-        $this->sub=& $sub;
-        $this->post_data=$all_data;
-        $judger=new JudgerModel();
-        $this->oid=OJModel::oid('hdu');
-        if(is_null($this->oid)) {
+        $this->sub = &$sub;
+        $this->post_data = $all_data;
+        $judger = new JudgerModel();
+        $this->oid = OJModel::oid('hdu');
+        if (is_null($this->oid)) {
             throw new Exception("Online Judge Not Found");
         }
-        $judger_list=$judger->list($this->oid);
-        $this->selectedJudger=$judger_list[array_rand($judger_list)];
+        $judger_list = $judger->list($this->oid);
+        $this->selectedJudger = $judger_list[array_rand($judger_list)];
     }
 
     private function _login()
     {
-        $response=$this->grab_page('http://contest-hunter.org:83', 'contesthunter', [], $this->selectedJudger["handle"]);
-        if (strpos($response, '登录')!==false) {
+        $response = $this->grab_page([
+            'site' => 'http://contest-hunter.org:83',
+            'oj' => 'contesthunter',
+            'handle' => $this->selectedJudger["handle"],
+        ]);
+        if (strpos($response, '登录') !== false) {
             preg_match('/<input name="CSRFToken" type="hidden" value="([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"\/>/', $response, $match);
-            $token=$match[1];
+            $token = $match[1];
 
-            $params=[
+            $params = [
                 'CSRFToken' => $token,
                 'username' => $this->selectedJudger["handle"],
                 'password' => $this->selectedJudger["password"],
                 'keepOnline' => 'on',
             ];
-            $this->login('http://contest-hunter.org:83/login', http_build_query($params), 'contesthunter', false, $this->selectedJudger["handle"]);
+            $this->login([
+                'url' => 'http://contest-hunter.org:83/login',
+                'data' => http_build_query($params),
+                'oj' => 'contesthunter',
+                'handle' => $this->selectedJudger["handle"],
+            ]);
         }
     }
 
     private function _submit()
     {
-        $response=$this->grab_page("http://contest-hunter.org:83/contest/{$this->post_data['cid']}/{$this->post_data['iid']}", "contesthunter", [], $this->selectedJudger["handle"]);
+        $response = $this->grab_page([
+            'site' => "http://contest-hunter.org:83/contest/{$this->post_data['cid']}/{$this->post_data['iid']}",
+            'oj' => "contesthunter",
+            'handle' => $this->selectedJudger["handle"],
+        ]);
 
         preg_match('/<input name="CSRFToken" type="hidden" value="([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"\/>/', $response, $match);
-        $token=$match[1];
+        $token = $match[1];
 
-        $params=[
+        $params = [
             'CSRFToken' => $token,
             'language' => $this->post_data["lang"],
             'code' => base64_encode(mb_convert_encoding($this->post_data["solution"], 'utf-16', 'utf-8')),
         ];
-        $response=$this->post_data("http://contest-hunter.org:83/contest/{$this->post_data['cid']}/{$this->post_data['iid']}?submit", http_build_query($params), "contesthunter", true, false, true, false, [], $this->selectedJudger["handle"]);
+        $response = $this->post_data([
+            'site' => "http://contest-hunter.org:83/contest/{$this->post_data['cid']}/{$this->post_data['iid']}?submit",
+            'data' => http_build_query($params),
+            'oj' => "contesthunter",
+            'ret' => true,
+            'returnHeader' => true,
+            'handle' => $this->selectedJudger["handle"],
+        ]);
         if (preg_match('/\nLocation: \/record\/(\d+)/i', $response, $match)) {
-            $this->sub['remote_id']=$match[1];
+            $this->sub['remote_id'] = $match[1];
         } else {
-            $this->sub['verdict']='Submission Error';
+            $this->sub['verdict'] = 'Submission Error';
         }
     }
 
     public function submit()
     {
-        $validator=Validator::make($this->post_data, [
+        $validator = Validator::make($this->post_data, [
             'pid' => 'required|integer',
             'cid' => 'required',
             'coid' => 'required|integer',
@@ -75,7 +96,7 @@ class Submitter extends Curl
         ]);
 
         if ($validator->fails()) {
-            $this->sub['verdict']="System Error";
+            $this->sub['verdict'] = "System Error";
             return;
         }
 
